0db7a7881c9d2d15e91d1e250d85bb22c4d0527d
daniel <daniel.jang@programmer.net>
2017-11-06 01:57:34 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+export class MyClass {
+
+  private v1;
+  private v2;
+
+  constructor(obj: {s1, s2}) {
+    this.v1 = obj.s1;
+    this.v2 = obj.s2;
+   }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+
+  title = `Angular 5 App`;
+  o1 = new MyClass( {'s1': 1, 's2': 2} );
 }
